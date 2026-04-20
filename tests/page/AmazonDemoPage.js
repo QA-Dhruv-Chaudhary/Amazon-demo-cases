1aa9aa82865b0e67d0d9067c872389e0af725269
DN-Chaudhary-018 <dhavaln1236@gmail.com>
2026-04-20 16:34:20 +0530

    fix: stabilize tests

--- tests/page/AmazonDemoPage.js
+++ tests/page/AmazonDemoPage.js
@@ -4,40 +4,70 @@ const { amazonDemoLocators } = require('../locator/amazonDemo.locator');
 class AmazonDemoPage {
   constructor(page) {
     this.page = page;
+    this.activePage = page;
   }
 
   async open(url = '/') {
     console.log(`[AmazonDemoPage] Opening URL: ${url}`);
     await this.page.goto(url);
+    await this.page.waitForLoadState('domcontentloaded');
+    this.activePage = this.page;
   }
 
   async amazonDemo(searchQuery) {
     console.log(`[AmazonDemoPage] Performing search with query: ${searchQuery}`);
     await this.page.locator(amazonDemoLocators.searchInput).fill(searchQuery);
-    await this.page.locator(amazonDemoLocators.searchButton).click();
+    await Promise.all([
+      this.page.waitForLoadState('domcontentloaded'),
+      this.page.locator(amazonDemoLocators.searchButton).click(),
+    ]);
   }
 
   async expectAmazonDemoSuccess() {
     console.log('[AmazonDemoPage] Verifying search results are visible');
     await expect(this.page.locator(amazonDemoLocators.compareSearchResult).first()).toBeVisible();
+    await expect(this.page.locator(amazonDemoLocators.fourthFromList)).toBeVisible();
   }
 
   async selectFourthFromList() {
     console.log('[AmazonDemoPage] Selecting fourth from list');
-    const fourthItem = this.page.locator(amazonDemoLocators.fourthFromList);
-    await this.page.waitForTimeout(5000);
-    await fourthItem.click();
+    const fourthItem = this.page.locator(amazonDemoLocators.fourthFromList).first();
+    await fourthItem.waitFor({ state: 'visible' });
+
+    const [productPage] = await Promise.all([
+      this.page.context().waitForEvent('page'),
+      fourthItem.click(),
+    ]);
+
+    await productPage.waitForLoadState('domcontentloaded');
+    this.activePage = productPage;
+  }
+
+  async addToCart() {
+    console.log('[AmazonDemoPage] Adding item to cart');
+    const addToCartButton = this.activePage.locator(amazonDemoLocators.addToCart);
+    await addToCartButton.waitFor({ state: 'visible' });
+    await Promise.all([
+      this.activePage.waitForLoadState('domcontentloaded'),
+      addToCartButton.click(),
+    ]);
   }
 
   async goToCart() {
     console.log('[AmazonDemoPage] Going to cart');
-    await this.page.locator(amazonDemoLocators.goToCart).click();
+    const goToCartLink = this.activePage.locator(amazonDemoLocators.goToCart);
+    await goToCartLink.waitFor({ state: 'visible' });
+    await Promise.all([
+      this.activePage.waitForLoadState('domcontentloaded'),
+      goToCartLink.click(),
+    ]);
   }
 
   async deleteFromCart() {
-    
     console.log('[AmazonDemoPage] Deleting from cart');
-    await this.page.locator(amazonDemoLocators.deleteFromCart).click();
+    const deleteFromCartButton = this.activePage.locator(amazonDemoLocators.deleteFromCart).first();
+    await deleteFromCartButton.waitFor({ state: 'visible' });
+    await deleteFromCartButton.click();
   }
 
   async testCase1() {
@@ -51,6 +81,7 @@ class AmazonDemoPage {
   }
   async testCase3() {
     await this.testCase2();
+    await this.addToCart();
     await this.goToCart();
     await this.deleteFromCart();
   } 
