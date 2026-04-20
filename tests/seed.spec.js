533f2674a067c9c1b79915a9544d4a15a76553f1
DN-Chaudhary-018 <dhavaln1236@gmail.com>
2026-04-20 18:09:05 +0530

    test demo

--- tests/seed.spec.js
+++
@@ -1,7 +0,0 @@
-const { test } = require('@playwright/test');
-
-test.describe('Seed / initialization', () => {
-  test('project setup placeholder', async () => {
-    test.skip(true, 'Add your environment or data setup steps here.');
-  });
-});
